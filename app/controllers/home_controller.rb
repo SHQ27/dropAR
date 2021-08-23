b1a82a391916f8655e43f5fb5c874c2c38356ff1
SHQ <sebastian@idiotlab.io>
2021-08-23 03:05:51 -0300

    FilterTester en root de la web

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
+
   def index
+    client_id = params[:client].to_i
+    product_code = params[:filter]
+
+    if client_id && product_code
+      selectedFilter = Filter.find_by(product_code: product_code)
+      if selectedFilter.client.id == client_id
+        @filter = selectedFilter
+      end
+    end
+    @clients = Client.all
   end
+
 end
