0cf6f5030e68c08c0485731f036b5130f25b666f
Sebastián Hernández Quiroga <shernandez@sneakersapps.com>
2022-03-06 23:56:28 -0300

    Filter paths to ar_paths

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:send_mail]
   def index
-    @escritorioQR = QrService.urlToQR(filters_path(:client => 'SITE', :filter => 'site0', :ar => 'true'), 'ar0')
-    @ar1QR = QrService.urlToQR(filters_path(:client => 'SITE', :filter => 'site1', :ar => 'true'), 'ar1')
-    @ar2QR = QrService.urlToQR(filters_path(:client => 'SITE', :filter => 'site2', :ar => 'true'), 'ar2')
-    @ar3QR = QrService.urlToQR(filters_path(:client => 'SITE', :filter => 'site3', :ar => 'true'), 'ar3')
-    @ar4QR = QrService.urlToQR(filters_path(:client => 'SITE', :filter => 'site4', :ar => 'true'), 'ar4')
+    @escritorioQR = QrService.urlToQR(filters_ar_path(:client => 'SITE', :filter => 'site0', :ar => 'true'), 'ar0')
+    @ar1QR = QrService.urlToQR(filters_ar_path(:client => 'SITE', :filter => 'site1', :ar => 'true'), 'ar1')
+    @ar2QR = QrService.urlToQR(filters_ar_path(:client => 'SITE', :filter => 'site2', :ar => 'true'), 'ar2')
+    @ar3QR = QrService.urlToQR(filters_ar_path(:client => 'SITE', :filter => 'site3', :ar => 'true'), 'ar3')
+    @ar4QR = QrService.urlToQR(filters_ar_path(:client => 'SITE', :filter => 'site4', :ar => 'true'), 'ar4')
   end
 
   def send_mail
