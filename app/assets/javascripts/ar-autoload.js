e8c3021e62b5467c4e159dc9ced6e543b199a629
Sebastián Hernández Quiroga <shernandez@sneakersapps.com>
2022-04-06 19:18:42 -0300

    Android keep debugging

--- app/assets/javascripts/ar-autoload.js
+++ app/assets/javascripts/ar-autoload.js
@@ -55,7 +55,7 @@ $(document).ready(function() {
             if (!iOS) {
                 checkedCompatibility = false;
                 document.getElementById('AndroidLink').click();
-                await new Promise(r => setTimeout(r, 5000));
+                setTimeout(function(){}, 5000);
                 if (!playStoreAccessed && confirm('Para acceder al contenido debe descargar un componente de Google Play ¿Desea proceder?')) {
                     localStorage.setItem('playStoreAccessed', Date.now());
                     checkedCompatibility = true;
